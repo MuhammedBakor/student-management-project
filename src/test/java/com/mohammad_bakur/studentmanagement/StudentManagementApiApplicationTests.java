98bbb430fa7ea447a81a0eec48590fb02ff0d2ea
Mohammad Bakur <assaa1122a@gmail.com>
2024-05-18 11:32:11 +0300

    Initializing Student management project

---
+++ src/test/java/com/mohammad_bakur/studentmanagement/StudentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammad_bakur.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
